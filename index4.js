970e8992ffd3d5d6915ec4bda984dda932ba7fe2
Nguyenphangiang <phancruel@gmail.com>
2022-01-16 21:15:09 +0700

    up4

---
+++ index4.js
@@ -0,0 +1,11 @@
+function addNumbers() {
+    firstNum = 4;
+    secondNum = 8;
+    result = firstNum + secondNum;
+    alert(result)
+    return result;
+}
+result = addNumbers();
+result = 0;
+alert(result)
+
